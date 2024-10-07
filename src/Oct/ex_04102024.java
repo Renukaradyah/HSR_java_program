1a851b86e80d802f3869ee635e04a1384952dd4d
Renukaradhya <rradhyhiremath@gmail.com>
2024-10-07 20:24:24 +0530

    test

---
+++ src/Oct/ex_04102024.java
@@ -0,0 +1,10 @@
+package Oct;
+
+public class ex_04102024 {
+    public static void main(String[] args){
+        System.out.println("10+10");
+        System.out.println("10-10");
+        System.out.println("10*10");
+        System.out.println("10/10");
+    }
+}
